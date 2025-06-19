49c9ed5ae0f3a7fb8424a87b3939ebb10afd0437
Niklas Marion <niklas.marion@gmx.de>
2025-06-19 14:52:39 +0200

    provide unique keys as argument to check

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ if __name__ == "__main__":
         '--create-duplicate', '-D', help='Create the same image two times with suffix', default=False, required=False, action='store_true')
     argparse.add_argument('--create-pdf', '-P', help='Create a pdf file', default=False, required=False, action='store_true')
 
+    argparse.add_argument("--unique-keys", "-u", help="Unique keys in the CSV file, comma separated", default="lastname,firstname", required=False)
+
     args = argparse.parse_args()
 
     data_file = args.data
@@ -28,6 +30,7 @@ if __name__ == "__main__":
     footer_file = args.footer
     create_duplicate = args.create_duplicate
     create_pdf = args.create_pdf
+    unique_keys = args.unique_keys.split(',')
 
     df = pd.read_csv(data_file, sep=args.seperator)
 
@@ -39,10 +42,12 @@ if __name__ == "__main__":
 
         text = text.replace('-', '-\n').replace(' ', '\n')
 
-        # check if lastname is duplicated in the csv file preprend the firstname initial
-        #if df[df['lastname'] == lastname].shape[0] > 1:
-        #   text = firstname[0].upper() + ". " + lastname.upper()
-        #   print("Duplicated lastname", lastname, "prepending firstname initial", firstname[0])
+        is_unique = df[df[unique_keys].eq(row[unique_keys]).all(axis=1)].shape[0] == 1
+        if not is_unique:
+            print("Warning: Duplicate found for", row[unique_keys].to_dict())
+            text = text = firstname[0].upper() + ". " + lastname.upper()
+            print("Appending first letter of firstname to text:", text)
+            continue
         
         header_file = row['header']
         output_file = output_folder + "/" + lastname + "_" + firstname
